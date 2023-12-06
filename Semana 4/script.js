e54ad9ab6aee5506fa495383da73d31c5b165c4e
Marcos <mvtgomes.ege@uesc.br>
2023-12-06 20:50:14 -0300

    alterações

---
+++ Semana 4/script.js
@@ -0,0 +1,51 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Pacotes de Viagem</title>
+</head>
+<body>
+    <div class="container">
+        <div class="roteiro">
+            <img src="https://blog.uber-cdn.com/cdn-cgi/image/width=2160,quality=80,onerror=redirect,format=auto/wp-content/uploads/2019/01/Encante-se-com-alguns-dos-lugares-mais-bonitos-em-Salvador.png" alt="Salvador">
+            <h1>Pacote Salvador</h1>
+            <ul>
+                <li>Passagem aérea ida e volta</li>
+                <li>03 noites de hospedagem</li>
+                <li>Café da manhã incluso</li>
+            </ul>
+            <h2>R$850,00</h2>
+            <p>Taxas Inclusas <br> Em até 12x sem Juros</p>
+            <button id="btn-comprar-salvador">Comprar</button>
+        </div>
+        <div class="roteiro">
+            <img src="https://www.viagio.com.br/uploads/images/cidades/fortaleza/fortaleza.jpg" alt="Fortaleza">
+            <h1>Pacote Fortaleza</h1>
+            <ul>
+                <li>Passagem aérea ida e volta</li>
+                <li>07 noites de hospedagem</li>
+                <li>Café da manhã incluso</li>
+            </ul>
+            <h2>R$1900,00</h2>
+            <p>Taxas Inclusas <br> Em até 12x sem Juros</p>
+            <button id="btn-comprar-fortaleza">Comprar</button>
+        </div>
+        <div class="roteiro">
+            <img src="https://s2-g1.glbimg.com/rlVMNKoOPEBVhlySrdMwgyfKG9Y=/0x0:1000x664/984x0/smart/filters:strip_icc()/i.s3.glbimg.com/v1/AUTH_59edd422c0c84a879bd37670ae4f538a/internal_photos/bs/2017/p/W/rZPacUTXeOBYrFGAAnBA/campinas.jpg" alt="Campinas">
+            <h1>Pacote Campinas</h1>
+            <ul>
+                <li>Passagem aérea ida e volta</li>
+                <li>04 noites de hospedagem</li>
+                <li>Café da manhã incluso</li>
+            </ul>
+            <h2>R$1200,00</h2>
+            <p>Taxas Inclusas <br> Em até 12x sem Juros</p>
+            <button id="btn-comprar-campinas">Comprar</button>
+        </div>
+    </div>
+
+    <script src="./script.js"></script>
+</body>
+</html>
